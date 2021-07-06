4a643c1d71e032b930f56524e26c4cb30bd4e113
Navruzbek20 <61547643+Navruzbek20@users.noreply.github.com>
2021-07-06 10:55:50 +0500

    boshlangich

---
+++ history.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Thu Jul  1 10:26:15 2021)---
+runfile('C:/Users/user/.spyder-py3/temp.py', wdir='C:/Users/user/.spyder-py3')
+
+## ---(Mon Jul  5 18:40:36 2021)---
+runfile('E:/Python/Lugat.py', wdir='E:/Python')
+
+## ---(Tue Jul  6 09:45:56 2021)---
+runfile('C:/Users/user/.spyder-py3/temp.py', wdir='C:/Users/user/.spyder-py3')
---
+++ temp.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+sonlar = list(range(1,11))
+for son in sonlar:
+    if son == 5:
+        break
+    print(f"{son} ning kvadrati {son**2} ga teng")
